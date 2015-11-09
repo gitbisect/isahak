78ec680fcbc421779015c5754cc07b7c472f043e
John Varghese <lookitup4me@gmail.com>
2015-11-09 09:29:00 -0800

    2 examples 0 failures, runs automatically with guard in color

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -100,6 +100,8 @@ require 'rspec'
 
 require_relative '../lib/models/list'
 require_relative '../lib/models/node'
+require_relative '../spec/factories/lists.rb'
+require_relative '../spec/factories/nodes.rb'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
